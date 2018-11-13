ddda1766a8ec4fc931bf225fbf011e4204943989
Christopher Fredén <c.freden@gmail.com>
2018-11-13 12:52:22 +0100

    Bump minor version

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
   inherit (lib) cleanSource makeBinPath;
   inherit (stdenv) mkDerivation isLinux;
 in mkDerivation rec {
-  version = "0.0.3";
+  version = "0.1.0";
   name = "pairon-${version}";
   buildInputs = [ makeWrapper ];
   src = cleanSource ./.;
